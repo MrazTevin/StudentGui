57bdf31b6305c7b2e6e359d5abee82094dc8e0d4
MrazTevin <millatevin@gmail.com>
2021-05-29 07:43:36 +0300

    recreated new files

---
+++ src/com/codewithTev/ConnectDb.java
@@ -0,0 +1,5 @@
+package com.codewithTev;
+
+public class ConnectDb {
+    
+}
---
+++ src/com/codewithTev/StudentGui.java
@@ -0,0 +1,18 @@
+package com.codewithTev;
+
+import javax.swing.*;
+
+public class StudentGui {
+    private JPanel StudentForm;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton SAVEButton;
+    private JButton DELETEButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
